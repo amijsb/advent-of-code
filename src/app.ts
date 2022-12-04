bbf42a4a7810fdbdc54c0c92ec75d649197ad4be
amijsb <amijsbreunesse@gmail.com>
2022-12-04 12:42:34 +0100

    Solve day 04

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
-import { part01 } from "./2022/day-01/01";
-import { part02 } from "./2022/day-01/02";
+import { part01 } from "./2022/day-04/01";
+import { part02 } from "./2022/day-04/02";
 
-const input = "./input/input-01.txt";
+const year = "2022";
+const day = "day-04";
+
+const input = `./input/${year}/${day}/input.txt`;
+const testinput = `./input/${year}/${day}/test-input.txt`;
 
 console.log(`The answer to part 1 is ${part01(input)}. The answer to part 2 is ${part02(input)}.`);
