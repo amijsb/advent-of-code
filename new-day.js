2aa6fb2c21d214ddd5a4b16b547d2208a65b7631
amijsb <amijsbreunesse@gmail.com>
2022-12-05 16:07:16 +0100

    Fix generator

--- new-day.js
+++ new-day.js
@@ -10,44 +10,48 @@ const part01 = fs.readFileSync(path.join(__dirname, `templates/01.ts`), "utf-8")
 const part02 = fs.readFileSync(path.join(__dirname, `templates/02.ts`), "utf-8");
 
 const createNewDay = (year, day) => {
-  fs.mkdir(path.join(__dirname, `input/${year}/${day}`), (err) => {
+  fs.mkdir(path.join(__dirname, `input/${year}/${day}`), { recursive: true }, (err) => {
     if (err) {
       return console.error(err);
     }
+
+    fs.writeFile(
+      path.join(__dirname, `input/${year}/${day}/input.txt`),
+      "[insert input]",
+      (err) => {
+        if (err) {
+          return console.error(err);
+        }
+      },
+    );
+
+    fs.writeFile(
+      path.join(__dirname, `input/${year}/${day}/test-input.txt`),
+      "[insert test input]",
+      (err) => {
+        if (err) {
+          return console.error(err);
+        }
+      },
+    );
   });
 
-  fs.mkdir(path.join(__dirname, `src/${year}/${day}`), (err) => {
+  fs.mkdir(path.join(__dirname, `src/${year}/${day}`), { recursive: true }, (err) => {
     if (err) {
       return console.error(err);
     }
-  });
 
-  fs.writeFile(path.join(__dirname, `input/${year}/${day}/input.txt`), "[inset input]", (err) => {
-    if (err) {
-      return console.error(err);
-    }
-  });
-
-  fs.writeFile(
-    path.join(__dirname, `input/${year}/${day}/test-input.txt`),
-    "[insert test input]",
-    (err) => {
+    fs.writeFile(path.join(__dirname, `src/${year}/${day}/01.ts`), part01, (err) => {
       if (err) {
         return console.error(err);
       }
-    },
-  );
+    });
 
-  fs.writeFile(path.join(__dirname, `src/${year}/${day}/01.ts`), part01, (err) => {
-    if (err) {
-      return console.error(err);
-    }
-  });
-
-  fs.writeFile(path.join(__dirname, `src/${year}/${day}/02.ts`), part02, (err) => {
-    if (err) {
-      return console.error(err);
-    }
+    fs.writeFile(path.join(__dirname, `src/${year}/${day}/02.ts`), part02, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+    });
   });
 
   console.log(`Created ${day} for you!`);
