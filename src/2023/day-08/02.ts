4d565b85553ece7743647ee3baf2f4fec0d7c3a9
amijsb <amijsbreunesse@gmail.com>
2023-12-25 14:58:28 +0100

    Clean up 2023/day-08

--- src/2023/day-08/02.ts
+++ src/2023/day-08/02.ts
@@ -1,10 +1,5 @@
 import { getInput, navigateWasteland, Nodes } from "./01";
-
-const findLCMOfArray = (numbers: number[]): number => {
-  const gcd = (x: number, y: number): number => (y === 0 ? x : gcd(y, x % y));
-  const lcm = (a: number, b: number): number => (a * b) / gcd(a, b);
-  return numbers.reduce(lcm);
-};
+import { findLCM } from "../../../helpers/lowest-common-denominator";
 
 const ghostSteps = (instructions: string, nodes: Nodes) => {
   const currentPositions = Object.keys(nodes).filter((key) => key.endsWith("A"));
@@ -13,7 +8,7 @@ const ghostSteps = (instructions: string, nodes: Nodes) => {
     navigateWasteland(instructions, nodes, position, "Z"),
   );
 
-  return findLCMOfArray(stepsPerPosition);
+  return findLCM(stepsPerPosition);
 };
 
 export const part02 = (file: string) => {
