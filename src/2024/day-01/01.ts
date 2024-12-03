ba80bf795228ba548d9f7b0cb66000a0cfbdb913
amijsb <amijsbreunesse@gmail.com>
2024-12-03 17:16:15 +0100

    Clean up 2024/day-01

--- src/2024/day-01/01.ts
+++ src/2024/day-01/01.ts
@@ -3,25 +3,22 @@ import { readFileSync } from "fs";
 export const getInput = (file: string) => {
   const fileContent = readFileSync(file, "utf-8");
 
-  const input = fileContent.split("\n");
+  return fileContent.split("\n").reduce(
+    (acc: { left: number[]; right: number[] }, line) => {
+      const [a, b] = line.split("   ");
 
-  const left: number[] = [];
-  const right: number[] = [];
+      acc.left = [...acc.left, Number(a)];
+      acc.right = [...acc.right, Number(b)];
 
-  input.forEach((line) => {
-    const [a, b] = line.split("   ");
-
-    left.push(Number(a));
-    right.push(Number(b));
-  });
-
-  return { left, right };
+      return acc;
+    },
+    { left: [], right: [] },
+  );
 };
 
 export const part01 = (file: string) => {
   const { left, right } = getInput(file);
 
   const distances = left.sort().map((value, index) => Math.abs(value - right.sort()[index]));
-
   return distances.reduce((a, b) => a + b, 0);
 };
--- src/2024/day-01/02.ts
+++ src/2024/day-01/02.ts
@@ -3,11 +3,6 @@ import { getInput } from "./01";
 export const part02 = (file: string) => {
   const { left, right } = getInput(file);
 
-  const similarityScores = left.map((value) => {
-    const count = right.filter((x) => x === value).length;
-
-    return count * value;
-  });
-
+  const similarityScores = left.map((value) => right.filter((x) => x === value).length * value);
   return similarityScores.reduce((a, b) => a + b, 0);
 };
